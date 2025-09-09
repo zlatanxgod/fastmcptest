0b3e9f65d845e4627c7286ac775b3d3b2349e9d8
Abhishant <abhishantsingh05@gmail.com>
2025-09-09 10:59:08 +0530

    added description

--- server.py
+++ server.py
@@ -3,8 +3,14 @@ from fastmcp import FastMCP
 mcp = FastMCP("My MCP Server")
 
 @mcp.tool
-def greet() -> str:
-    return f"Hello, Abhishant!"
+def greet(name: str) -> str:
+    """
+    Greets a user by their name. Use this tool when you need to provide a personalized greeting.
+
+    Args:
+        name (str): The first name of the person to greet.
+    """
+    return f"Hello, {name}!"
 
 if __name__ == "__main__":
     mcp.run(transport="http", port=8000)
